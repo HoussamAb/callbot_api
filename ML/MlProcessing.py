531ca26f25600ca42fa8ecb56beed9d198ccf73e
HoussamAb <houssama9b7@gmail.com>
2021-06-11 12:12:42 +0100

    Config done

--- ML/MlProcessing.py
+++ ML/MlProcessing.py
@@ -1,57 +1,182 @@
+from django.contrib.sitemaps.views import x_robots_tag
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
+import nltk
 import numpy as np
+import string
+import re
+import time
 from sklearn.model_selection import train_test_split
 
+class FilesManager:
+    _testDataframe = None # inbenta Test
+    _allDataSet = None # Dataset_allquetions
+    def loadTestData(self,fullpath):
+        self._testDataframe = pd.read_csv(fullpath , delimiter = ";",encoding ='ansi')
+        return self._testDataframe
+        # chargement de donnée a partir de fichier fullpath/filename.CSV
 
+    def loadDataSet(self,fullpath):
+        self._allDataSet = pd.read_csv(fullpath , delimiter = ";",encoding ='ansi')
+        return self._allDataSet
+
+#               NLP PROCESSING
+class NlpProcessing:
+    stopword = nltk.corpus.stopwords.words('french')# All frensh Stopwords
+    ps = nltk.PorterStemmer()
+    wn = nltk.WordNetLemmatizer()
+    def clean_text(text):
+        text = "".join([word.lower() for word in text if word not in string.punctuation])
+        tokens = re.split('\W+', text)
+        #text = [ps.stem(word) for word in tokens if word not in stopword]
+        #text = [wn.lemmatize(word) for word in tokens]
+        return text
+
+#               SPLIT DATA
 class SplitDataset:
     _testData = {}
     _trainData = {}
 
-    def __init__(self, dataset=None, howMuch=None):
+    def __init__(self, dataset=None):
+        if dataset is None:
+            self.dataset = FilesManager.loadDataSet('data/dataset.csv')
         self.dataset = dataset
-        self.howMuch = howMuch
 
-    def split(self):
-        train_x, test_x, train_y, test_y = train_test_split(self.dataset[0], self.dataset[1], test_size=20,
-                                                            random_state=42)
+    def split(self,testSize,RandomStat):
+        train_x, test_x, train_y, test_y = train_test_split(self.dataset["Questions"], self.dataset["Reponses"], test_size=testSize,
+                                                            random_state=RandomStat)
         self._trainData['train_x'] = train_x
         self._trainData['train_y'] = train_y
         self._testData['test_x'] = test_x
         self._testData['test_y'] = test_y
 
-    def __getTrain(self):
+    def getTrain(self):
         return self._trainData
 
-    def __getTest(self):
+    def getTest(self):
         return self._testData
 
-
+#                   TRAIN MODEL
 class TrainModel:
+    _countvectorizer = CountVectorizer(ngram_range=(2,2), analyzer=NlpProcessing.clean_text)
+    _tfidftransformer = TfidfTransformer()
+    _classifier = None
 
     def __init__(self, classifier=None, datasetx=None, datasety=None):
-        self.classifier = classifier
+        self._classifier = classifier
         self.datasetx = datasetx
         self.datasety = datasety
-
+    def initPipeline(self):
+        text_clf = Pipeline([('vect',self._countvectorizer),
+                                ('tfidf', self._tfidftransformer),
+                                ('clf',self._classifier),])
+        return text_clf
     def train(self):
-        if self.classifier is None or self.datasetx is None or self.datasety is None:
+        if self._classifier is None or self.datasetx is None or self.datasety is None:
             return 1
-        model = self.classifier.train(self.datasetx, self.datasety)
+        pipe = self.initPipeline()
+        model = pipe.fit(X=self.datasetx,y=self.datasety)
         return model
 
+#                   BUILD METRICS
 class metrics:
-
     _model = None
     _datatest = None
+    _predicted = None
 
     def __init__(self, model=None, datatest=None):
         self._model = model
         self._datatest = datatest
 
     def modelScore(self):
-        predicted = self.model.predict(self._datatest['test_x'])
-        score = self.model.score(predicted, self._datatest['test_y'])
+        self._predicted = self._model.predict(self._datatest['test_x'])
+        #score = self.model.score(predicted, self._datatest['test_y'])
+        score = np.mean(self._predicted == self._datatest['test_y'])*100
+        return score
 
     def classificationMetrics(self):
-        #import
-        return None
+        if self._predicted is None:
+            self.modelScore()
+        from sklearn.metrics import classification_report
+        report = classification_report(self._datatest['test_y'], self._predicted)
+        return report
 
+    def accuracy(self):
+        from sklearn.metrics import accuracy_score
+        if self._predicted is None:
+            self._predicted = self._model.predict(self._datatest['test_x'])
+        print("la precision sur valeurs prédites égale : %.3f %%" %(accuracy_score(self._datatest['test_y'], self._predicted)*100))
+
+#                   Test Sur Fichier INBENTA
+class MakeTest:
+    df1=None
+    _algo = None
+    df2 = None
+    def __init__(self,algo,datatest,dataset):
+        self._algo = algo
+        self.df1 = datatest
+        self.df2 = dataset
+
+    def predict_statis(self):
+        return self._algo
+
+    def compare_dataframes(self,df1, df2):
+        l1 = []
+        l2 = []
+        l3 = []
+        l4 = []
+        for i,rowi in df1.iterrows():
+            for j,rowj in df2.iterrows():
+                if(str(rowi['Réponse Inbenta']).lower() == str(rowj['Questions']).lower()):
+                    l1.append(rowi['Question'])
+                    l2.append(rowi['Réponse Inbenta'])
+                    l3.append(rowj['Questions'])
+                    l4.append(rowj['Réponses'])
+        df = pd.DataFrame({
+            'Questions individu':l1,
+            'Question inbenta':l2,
+            'Question vrai':l3,
+            'Réponse vrai':l4
+        })
+        return df
+
+    def comp(self,df):
+        label = []
+        for i,row in zip(df['réponce algorithme'],df['Réponse vrai']):
+            str(i).replace("[", "").replace("]","")
+            if i == row:
+                label.append(1)
+            else:
+                label.append(0)
+        df['label'] = label
+    def moyenne(self,df):
+        som = 0
+        for i in df.label:
+            if i == 1:
+                som += 1
+        return "Moyenne est : %.4f %%" % ((som/len(df))*100)
+    def moyenne_temps(self,df):
+        som = 0
+        print('Le totale des tests : ',len(df['temps de réponse (s)']))
+        for i in df['temps de réponse (s)']:
+            som += i
+        return "La Moyenne de temps de réponses : %.4f" % (som/len(df))
+    #'Questions individu' 'Question vrai'
+    def doTest(self,df1,df2,colomnName):
+        data = self.compare_dataframes(df1,df2)
+
+        test_rep = []
+        temps_de_rep = []
+        for i in data[colomnName]:
+            start_time = time.time()
+            test_rep.append(self.predict_statis([i]))
+            temps_de_rep.append(time.time() - start_time)
+        data['réponce algorithme'] = test_rep
+        data['temps de réponse (s)'] = temps_de_rep
+
+        data.head(5)
+        self.comp(data)
+        self.moyenne(data)
+        self.moyenne_temps(data)
--- api/views.py
+++ api/views.py
@@ -1,13 +1,13 @@
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-
 from api.models import api as apimodel
 from django.views import View
 import csv, io
 from django.shortcuts import render
 from django.contrib import messages
 import json  # Create your views here.
+import ML.MlProcessing as ml
 
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -52,14 +52,22 @@ class api(View):
             }
             return render(request, template, prompt)
         data_set = csv_file.read().decode('ansi')
+        #with open('data/dataset.csv', 'w' , encoding='ansi') as output:
+        #    output.write(data_set)
         # setup a stream which is when we loop through each line we are able to handle a data in a stream
         io_string = io.StringIO(data_set)
         next(io_string)
-        for column in csv.reader(io_string, delimiter=';'):
-            _, created = apimodel.objects.update_or_create(
-                questions=column[0],
-                reponses=column[1],
-            )
+        with open('data/dataset.csv', 'w', encoding='ansi', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(["Questions","Reponses"])
+            for column in csv.reader(io_string, delimiter=';'):
+                writer.writerow([column[0],column[1]])
+
+        #for column in csv.reader(io_string, delimiter=';'):
+        #    _, created = apimodel.objects.update_or_create(
+        #        questions=column[0],
+        #        reponses=column[1],
+        #    )
         context = {}
         return render(request, template, context)
 
@@ -74,12 +82,22 @@ class api(View):
             }
             return render(request, template, prompt)
 
+        from sklearn.ensemble import RandomForestClassifier
+        df = ml.FilesManager.loadDataSet(self,fullpath='./data/dataset.csv')
+        split_data = ml.SplitDataset(df)
+        split_data.split(testSize=20,RandomStat=42)
+        training = ml.TrainModel(RandomForestClassifier(),split_data.dataset["Questions"],split_data.dataset["Reponses"])
+        model = training.train()
+        metric = ml.metrics(model,split_data.getTest())
+
+
         template = "csv_loader.html"
         # data = apimodel.objects.all()
         # prompt is a context variable that can have different values      depending on their context
         prompt = {
             'order': 'Order of the CSV should be Questions, responses',
-            'profiles': None
+            'profiles': None,
+            'trained': metric.modelScore()
         }
         # GET request returns the value of the data with the specified key.
         return render(request, template, prompt)
